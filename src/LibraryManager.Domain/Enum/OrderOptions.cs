a0cd510f19f22cfecd488bb4b5447d5fdf63c318
Konrad <231018054+K-SCode@users.noreply.github.com>
2026-03-22 02:41:54 +0100

    feat: add Servives and Service Interfaces for: Book, Author, Rental, Category.

---
+++ src/LibraryManager.Domain/Enum/OrderOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Domain.Enum
+{
+    public enum OrderOptions
+    {
+        ASC,
+        DESC
+    }
+}
