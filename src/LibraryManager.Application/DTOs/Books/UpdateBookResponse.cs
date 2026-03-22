f39ed5c896855b7d34a888afcd052057a49f4908
Konrad <231018054+K-SCode@users.noreply.github.com>
2026-03-22 03:08:07 +0100

    refactor: reorganize service and DTO structure and add new models for Books Categories and Authors

---
+++ src/LibraryManager.Application/DTOs/Books/UpdateBookResponse.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManager.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Application.Dtos.Books
+{
+    public record UpdateBookResponse(
+        Guid Id,
+        string Title,
+        Guid AuthorId,
+        string Isbn,
+        string Description,
+        BookStatus Status,
+        DateTime PublishDate,
+        Guid CategoryId,
+        Guid? CurrentBorrowerId,
+        DateTime? ReturnDate
+      );
+}
---
+++ src/LibraryManager.Application/DTOs/Categories/CreateCategoryResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Application.Dtos.Categories
+{
+    public record CreateCategoryResponse(
+        Guid Id,
+        string Name
+    );
+}
