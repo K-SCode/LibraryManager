35e2c8fa9f4e93fe2693161510fa1530d36c3a99
Konrad <231018054+K-SCode@users.noreply.github.com>
2026-03-22 01:12:27 +0100

    feat: add DTOs for Authors, Books, Categorries and Pagination

---
+++ LibraryManager.Application/DTOs/Authors/UpdateAuthorRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Application.Dtos.Authors
+{
+    public record UpdateAuthorRequest(
+        string FirstName,
+        string LastName
+    );
+}
---
+++ LibraryManager.Application/DTOs/Books/BookResponse.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManager.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Application.Dtos.Books
+{
+    public record BookResponse(
+        Guid Id,
+        string Title,
+        string Author,
+        string Isbn,
+        string Description,
+        BookStatus Status,
+        DateTime? ReturnDate,
+        DateTime PublishDate,
+        string CategoryName
+        );
+
+}
